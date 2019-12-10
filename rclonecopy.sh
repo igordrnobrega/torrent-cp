4cf10b0d3771b4012d264f21c0ef873fc294bcb4
Igor Dourado Reis Nóbrega <igor.nobrega@linio.com>
2019-12-09 18:08:53 -0600

    Fix script

--- rclonecopy.sh
+++ rclonecopy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+shopt -s extglob
 
 DATETIME=$(date '+%Y-%m-%d %H:%M:%S')
 
+# Catch the first argument to get the folder that will be analysed
 TORRENT_FOLDER="$1"
-TORRENT_DOWNLOAD_DIR="/Users/igornobrega/Movies/Movies"
+# Set the default directory where the folder will be downloaded
+TORRENT_DOWNLOAD_DIR=${2:-"/Users/igornobrega/Movies/Movies"}
+# Set the final directory
 TORRENT_FINAL_DIR="/Volumes/THAIS LEITE/Plex"
 TORRENT_PATH="$TORRENT_DOWNLOAD_DIR"/"$TORRENT_FOLDER"
 
@@ -14,6 +18,7 @@ log() {
     echo "[""$DATETIME""] "$1"" >> "$LOG_FILE_PATH"
 }
 
+# Move log file when it reaches a size limit
 move_log_file() {
     if [ -f "$LOG_FILE_PATH" ] && [ "$(stat -f%z "$LOG_FILE_PATH")" -gt 100000 ]; then
         log "Moving log file"
@@ -34,7 +39,7 @@ create_log_file() {
 create_final_path() {
     if ! [ -d "$TORRENT_FINAL_DIR" ]; then
         log "Creating folder "$TORRENT_FINAL_DIR""
-        mkdir "$TORRENT_DIR"
+        mkdir "$TORRENT_FINAL_DIR"
     fi
 }
 
@@ -47,21 +52,23 @@ create_final_path
 log "Processing: "$TORRENT_FOLDER""
 
 if [ -d "$TORRENT_PATH" ]; then
-    log "Cleaning up "$TORRENT_PATH" folder"
+    log "Cleaning up ""$TORRENT_PATH"" folder"
 
-    find "$TORRENT_PATH" -type f | grep -vE '\.(mp4|avi|mkv)$' | xargs rm -f
+    # find "$TORRENT_PATH" -type f | grep -vE '\.(mp4|avi|mkv|str)$' | xargs rm -f
+
+    find "$TORRENT_PATH" -type f -not \( -name "*.mkv" -o -name "*.srt" -o -name "*.mp4" -o -name "*.avi" \) -delete
 
     FOLDER_NAME=$(echo "$TORRENT_FOLDER" | sed -e 's/.S.*//')
     TORRENT_FINAL_PATH="$TORRENT_FINAL_DIR"/"$FOLDER_NAME"
 
-    if ! [ -d"$TORRENT_FINAL_PATH" ]; then
-        log "Creating final path "$TORRENT_FINAL_PATH""
+    if ! [ -d "$TORRENT_FINAL_PATH" ]; then
+        log "Creating final path ""$TORRENT_FINAL_PATH"""
         mkdir "$TORRENT_FINAL_PATH"
     fi
 
-    log "Moving "$TORRENT_PATH" to "$TORRENT_FINAL_PATH""
+    log "Moving ""$TORRENT_PATH"" to ""$TORRENT_FINAL_PATH"""
 
-    mv "$TORRENT_PATH" "$TORRENT_FINAL_PATH"
+    mv "$TORRENT_PATH"/ "$TORRENT_FINAL_PATH"/
 else
-    log "Path not found "$TORRENT_PATH""
+    log "Path not found ""$TORRENT_PATH"""
 fi
